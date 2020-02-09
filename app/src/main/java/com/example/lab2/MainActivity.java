92c2c1d009b0d845114e818b28bf72bbf37987d1
nilpilihp <56451950+nilpilihp@users.noreply.github.com>
2020-02-08 19:07:44 -0600

    step 17 completed

--- app/src/main/java/com/example/lab2/MainActivity.java
+++ app/src/main/java/com/example/lab2/MainActivity.java
@@ -3,9 +3,29 @@ package com.example.lab2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    public void clickFunction(View view)
+    {
+        Log.i("Info","Button pressed");
+
+        EditText myTextField = (EditText) findViewById(R.id.textField);
+        // getText() get the text that the user entered in the text field
+        //Toast.makeText(MainActivity.this, myTextField.getText().toString(), Toast.LENGTH_LONG).show();
+        String str = myTextField.getText().toString();
+        goToActivity2(str);
+
+    }
+
+   public void goToActivity2()
+    {
+
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
